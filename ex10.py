94b1cfb55c79962ddd4449398afba44c8753d6a8
benjamin.li <benjamin.li@cloudminds.com>
2017-12-21 18:31:16 +0800

    Day4: add exercise 8-14

---
+++ ex10.py
@@ -0,0 +1,17 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+slim_cat = "I'm \' a slim \" cat."
+
+fat_cat = ''' 
+    I'll do a list:
+    \t* Cat food
+    \t* Fishies
+    \t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print slim_cat
+print fat_cat
---
+++ ex11.py
@@ -0,0 +1,11 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+int_height = int(height)
+print "How much do you weight?",
+weight = raw_input()
+
+print "So, you are %r old, %r tall and %r heavy." % (
+        age, height, weight)
+print "int_height: %d" % int_height
